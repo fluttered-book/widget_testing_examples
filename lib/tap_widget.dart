71066b15067173a28187109d506a9aa726533e4b
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2025-02-25 14:03:49 +0100

    Easter eggs

--- lib/tap_widget.dart
+++ lib/tap_widget.dart
@@ -9,14 +9,19 @@ class TapWidget extends StatefulWidget {
 
 class _TapWidgetState extends State<TapWidget> {
   bool ok = false;
+
+  final tracks = [
+    "I'm Not Okay (I Promise) - MCR",
+    "Are You Really Okay? - Sleep Token",
+    "I Think I'm OKAY - mgk"
+  ]..shuffle();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Tap example")),
       body: Center(
-        child: ok
-            ? Text('Button was tapped')
-            : Text("I'm Not Okay (I Promise) - MCR"),
+        child: ok ? Text('Button was tapped') : Text(tracks[0]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
